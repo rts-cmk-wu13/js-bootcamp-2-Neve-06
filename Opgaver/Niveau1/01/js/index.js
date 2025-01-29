1ae686faf398892b2547c85b66d029d5b942532f
Neve-06 <6255234@rts-365.dk>
2025-01-29 12:38:05 +0100

    edited the colorBtn variable into a class. Added event to the btn and added an event.target.classlist.toggle, enabling the ability to make the button grey and or green

--- Opgaver/Niveau1/01/js/index.js
+++ Opgaver/Niveau1/01/js/index.js
@@ -1,3 +1,9 @@
 let Btn = document.querySelector("#my-button") 
-let colorBtn = document.querySelector("#my-button__green")
+let colorBtn = document.querySelector(".my-button__green")
 
+Btn.addEventListener("click", function(event){
+    console.log(event.target);
+    event.target.classList.toggle("my-button__green");
+    
+
+})

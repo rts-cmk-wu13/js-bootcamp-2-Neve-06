ba93bb875c81bf1b68b39f2d08ba8db1d52201f6
Neve-06 <6255234@rts-365.dk>
2025-01-30 11:25:08 +0100

    added function to prevent dividing by zero

--- Opgaver/Niveau1/02/js/division.js
+++ Opgaver/Niveau1/02/js/division.js
@@ -2,7 +2,16 @@
 function beregnForhold (a, b){
     return result = a / b;
 }
+function beregnForhold2 (a, b){
+    if (b == 0){
+        return null
+    } 
+    return result = a / b;
+}
 
 let forhold = beregnForhold (4, 3);
+
+let resultat2 = beregnForhold2( 12, 3)
+console.log(resultat2);
 
 console.info(forhold);
